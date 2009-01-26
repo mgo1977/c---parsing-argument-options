08cd26da233e576383698083e6c166091aec8674
Mariano Ortega <mgo1977@gmail.com>
2009-01-26 01:35:10 -0300

    Added LGPL license

--- Parser.h
+++ Parser.h
@@ -1,3 +1,30 @@
+/*
+ *   C++ Command Line Options Parser (yet another one! :~)
+ *
+ *   Copyright (C) 2009 Mariano Ortega  <mgo1977@gmail.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   You can freely clone it from:
+ *	git://github.com/mgo1977/c---parsing-argument-options.git
+ *
+ *   This is a very beta version, it has been developed in ~5 hours, so
+ *   probably you'll find some problems/bugs.
+ * 
+ *   Feel free to send me your feedback and comments (patches also are welcome)
+ *
+ */
 
 #ifndef __PARSER__
 #define __PARSER__
